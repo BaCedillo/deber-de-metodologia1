cb94a2b5aa4ef76d5d7de9338c1c549fe8ced5ad
BaCedillo <119425269+BaCedillo@users.noreply.github.com>
2023-01-24 20:28:46 -0500

    Add files via upload

---
+++ index/ejercicio2.js
@@ -0,0 +1,8 @@
+let age=prompt("ingresar edad");
+if(age<5 || age>65)
+{
+    alert ("accede a vacunacion")
+}
+else {
+    alert("no accede")
+}
---
+++ index/ejercicio3.js
@@ -0,0 +1,8 @@
+let age=prompt("ingrese edad");
+if(age>18 && age<35)
+{
+    alert ("bienvenido")
+}
+else {
+    alert("no pasar")
+}
